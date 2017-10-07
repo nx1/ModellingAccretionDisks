e0820813b9f8d17719f67afd4c3976a00d3bde59
Norman <norman1000@gmail.com>
2017-10-07 18:57:56 +0100

    Functions from chapter 5
    Added Some basic functions from chapter 5 Accretion Power in Astrophysics

---
+++ Chapter5Functions.py
@@ -0,0 +1,20 @@
+GRAVITATION = 10 #Gravitational constant G
+MASS = 50        #Mass of accreting star
+
+
+def AngularVelocity(R):
+    angVel = ((GRAVITATION * MASS)/R^4)**0.5
+    return angVel
+    
+def circularVelocity(R):
+  circVel = R*AngularVelocity(R)
+  return circVel
+  
+for r in range(1,10):
+  print('For a radius of', r, 'Star mass', MASS, 'the angular veloccity is', AngularVelocity(r))
+  print('The circular velocity is', circularVelocity(r))
+  print('-------')
+  
+  
+print('A larger radius creates a smaller angular velocity but a larger circular velocity')
+print('Both of these are of course independent of the radius of the star R*')
