90651787b8c80d2ca8325462601a4c32e76979f0
vs2g14 <vs2g14@soton.ac.uk>
2017-10-12 14:42:26 +0100

    Added temp function

--- Disk Creation.py
+++ Disk Creation.py
@@ -5,6 +5,8 @@ Created on Fri Oct  6 13:28:25 2017
 @author: yv
 """
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def create_disc (N,const,Rmax,rstart):
     R=[]
@@ -15,4 +17,17 @@ def create_disc (N,const,Rmax,rstart):
             R.append(R[i-1]*const)
     return np.asarray(R)        #Converts python list to numpy array
        
-print(create_disc(10,3,8,2))
+
+def tempeff (R):
+    temp=[]
+    for i in range(len(R)):
+        temp.append(R[i]**(-0.75))
+    return temp
+    
+    
+R=create_disc(10,3,8,2)
+t=tempeff(R)
+
+ax = plt.subplot(111)
+ax.set_xscale("log", nonposx='clip')
+ax.plot(R,t)
