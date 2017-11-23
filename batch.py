0eaf4d39d6e01900e55dc270b640d0bf5ca1f0b4
nx1 <norman1000@gmail.com>
2017-11-23 12:42:39 +0000

    batch

--- batch.py
+++ batch.py
@@ -3,4 +3,17 @@
 
 
 
-execfile("discfunc.py")
+#execfile("discfunc.py")
+
+
+N = [1,5,10,30]
+const = [1.1,1.5]
+Q_factor = [0.005,0.025,0.1]
+tMax_factor = [1.1,10.0]
+H_R_= [0.1,1.0]
+M_0_start = [1.0,10.0]
+
+map1 = map(None, N, const)
+
+for x, y in map1:
+    print x, y
