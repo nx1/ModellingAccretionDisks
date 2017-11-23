d2b73a4acb0d11adddb6b89cb16585ee69e537a7
vs2g14 <vs2g14@soton.ac.uk>
2017-11-23 13:32:57 +0000

    batch 2

--- batch2.py
+++ batch2.py
@@ -20,12 +20,12 @@ for i in range(77,len(biglist)):
     H_R_= biglist[i][4]
     M_0_start = biglist[i][5]
     
-    name0=N_l.index(N)
-    name1=const_l.index(const)
-    name2=Q_factor_l.index(Q_factor)
-    name3=tMax_factor_l.index(tMax_factor)
-    name4=H_R_l.index(H_R_)
-    name5=M_0_start_l.index(M_0_start)
+    name0 = N_l.index(N)
+    name1 = const_l.index(const)
+    name2 = Q_factor_l.index(Q_factor)
+    name3 = tMax_factor_l.index(tMax_factor)
+    name4 = H_R_l.index(H_R_)
+    name5 = M_0_start_l.index(M_0_start)
     
     NAME='%s%s%s%s%s%s.png' % (name0,name1,name2,name3,name4,name5)
     print NAME
